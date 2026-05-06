53ff724aeefdc3ff00c0a409c238cb458eae4ef4
Garulf <535299+Garulf@users.noreply.github.com>
2026-05-05 23:34:52 -0400

    Improve commands

--- steam_client/commands.py
+++ steam_client/commands.py
@@ -1,8 +1,5 @@
-from typing import List
 import webbrowser
 from enum import StrEnum
-
-SCHEME = 'steam'
 
 
 class SteamWindows(StrEnum):
@@ -23,47 +20,44 @@ class SteamWindows(StrEnum):
 
 class Command():
 
-    def __init__(self, scheme: str):
-        self._scheme = scheme
+    def _create_uri(self, *segments: str) -> str:
+        return f'steam://{"/".join(segments)}'
 
-    def _create_uri(self, path: List[str], endpoint: str) -> str:
-        return f'{self._scheme}://{"/".join(path)}/{endpoint}'
-
-    def __call__(self, path: List[str], endpoint: str) -> None:
+    def open(self, *segments: str) -> None:
         """Executes the command with the specified endpoint."""
-        webbrowser.open(self._create_uri(path, endpoint))
+        webbrowser.open(self._create_uri(*segments))
 
 
 class Commands:
     """A collection of commands for the Steam client."""
 
     def __init__(self):
-        self._command = Command(SCHEME)
+        self._command = Command()
 
     def run_game_id(self, app_id: str) -> None:
         """Launches game with the specified ID in the Steam client."""
-        self._command(['rungameid'], app_id)
+        self._command.open('rungameid', app_id)
 
     def store(self, app_id: str) -> None:
         """Opens the game's store page in the Steam client."""
-        self._command(['store'], app_id)
+        self._command.open('store', app_id)
 
     def install(self, app_id: str) -> None:
         """Opens the game's install prompt in the Steam client."""
-        self._command(['install'], app_id)
+        self._command.open('install', app_id)
 
     def uninstall(self, app_id: str) -> None:
         """Opens the game's uninstall prompt in the Steam client."""
-        self._command(['uninstall'], app_id)
+        self._command.open('uninstall', app_id)
 
     def update_news(self, app_id: str) -> None:
         """Opens the game's update news in the Steam client."""
-        self._command(['updatenews'], app_id)
+        self._command.open('updatenews', app_id)
 
     def open(self, window: SteamWindows) -> None:
         """Opens the specified window in the Steam client."""
-        self._command(['open'], window)
+        self._command.open('open', window)
 
     def open_url(self, url: str) -> None:
         """Opens the specified URL in the Steam client."""
-        self._command(['openurl'], url)
+        self._command.open('openurl', url)
