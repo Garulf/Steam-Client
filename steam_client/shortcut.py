f3e85362cc021b2562118d8aab5cad71757003a5
Garulf <535299+Garulf@users.noreply.github.com>
2026-07-09 15:30:14 -0400

    fix: update appid generation logic in Shortcut class

--- steam_client/shortcut.py
+++ steam_client/shortcut.py
@@ -57,10 +57,11 @@ class Shortcut(App):
     @cached_property
     def appid(self) -> str:
         """Returns the shortcut's generated 64-bit app ID."""
-        input_string = self._data["exe"] + self._data["appname"]
-        top_32 = CRC32_ALGORITHM.bit_by_bit(input_string) | SHORTCUT_TOP_BIT
+        stored_appid = self._data["appid"]
+        top_32 = int(stored_appid) & 0xFFFFFFFF
         full_64 = (top_32 << TOP32_SHIFT) | SHORTCUT_TAIL
         return str(full_64)
+
 
     @property
     def _short_id(self) -> str:
